e3441d3ad26c30ccaedc421319043c71cf96e575
Preisler <preisler.andras@gmail.com>
2023-04-05 18:17:09 +0200

    main

--- src/main/java/preislerdev/texteditor/HelloApplication.java
+++ src/main/java/preislerdev/texteditor/HelloApplication.java
@@ -1,9 +1,9 @@
 package preislerdev.texteditor;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,11 +11,15 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-
         Group group = new Group();
         Scene scene = new Scene(group, 320, 240);
+        Image icon = new Image("icon.png");
         stage.setTitle("Hello!");
         stage.setScene(scene);
+        stage.setResizable(false);
+        stage.centerOnScreen();
+
+        stage.getIcons().add(icon);
         stage.show();
     }
 
