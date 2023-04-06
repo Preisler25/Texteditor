5754da8aa314b746d308dda5b807360f2012d2ca
Preisler <preisler.andras@gmail.com>
2023-04-06 15:42:05 +0200

    main

--- src/main/java/preislerdev/texteditor/HelloApplication.java
+++ src/main/java/preislerdev/texteditor/HelloApplication.java
@@ -1,13 +1,16 @@
 package preislerdev.texteditor;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.nio.charset.Charset;
 
 public class HelloApplication extends Application {
     @Override
@@ -15,14 +18,17 @@ public class HelloApplication extends Application {
         int width = 1280;
         int height = 720;
 
-        TextField textField = new TextField();
+        TextField textField = new TextField("alma");
         textField.setLayoutX(width/ 2 - 100);
         textField.setLayoutY(height / 2 - 25);
         textField.setPrefWidth(200);
         textField.setPrefHeight(50);
 
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/example.fxml"));
+        VBox root = loader.load();
+
         Image icon = new Image("icon.png");
-        Group group = new Group(textField);
+        Group group = new Group(textField, root);
         Scene scene = new Scene(group, width, height);
 
 
@@ -37,5 +43,9 @@ public class HelloApplication extends Application {
 
     public static void main(String[] args) {
         launch();
+    }
+
+    public void handleButtonAction() {
+        System.out.println("Button clicked!");
     }
 }
